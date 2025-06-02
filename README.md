3e2c1d0707fefc0114dd8b9d40e477c1d2f0f485
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-06-03 01:40:24 +0300

    intro

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # My-Blog
+
+I thought python was a complicated language. Tough. Intimidating. Like everyone claimed.
+
+Once you sit with it, give it time and understand it, it's smoother than flaxseed gel on freshly dampened hair.
+
+This blog is a revision, visiting concepts, one variable, one print statement and one truth at a time.
---
+++ basics.py/variables_print.py
@@ -0,0 +1,15 @@
+ #variabe is a container where you throw in whatever value and call when you need
+# print is the 'voice'  
+
+name = "Renee"  #sting
+age = 23 #integer
+height = 5.8 #float
+is_developer = True #boolean
+with_cats = True
+
+
+print("My name is", name)
+print("I am", age, "years old")
+print("I stand at", height)
+print(is_developer)
+print(with_cats)
